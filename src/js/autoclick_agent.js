bf0df2bffffd752c044511d84705469d9cc5f253
robbiemu <roberto.tomas.cuentas@gmail.com>
2016-07-05 22:01:41 -0400

    v1.0

---
+++ src/js/autoclick_agent.js
@@ -0,0 +1,22 @@
+/* global $ */
+class AutoclickAgent {
+  constructor () {
+    this.base_time = 1
+  }
+
+  act () {
+    if (!this.cancel) {
+      setTimeout(() => {
+        increaseAutoclickCost()
+        $('#per_click').trigger('click')
+        this.act()
+      }, this.get_next_trigger_timestamp())
+    }
+  }
+
+  get_next_trigger_timestamp () {
+    let next = Math.floor((this.base_time + ((Math.random() - Math.random()) / 10)) * 1000)
+    console.log('next trigger in ' + next + 'ms')
+    return next
+  }
+}
--- src/js/game.js
+++ src/js/game.js
@@ -1,35 +1,94 @@
-/* global $, GameEngine */
+/* global $, AutoclickAgent */
 $().ready(() => {
   console.log('loading')
   game_init()
   $('.button').click(generic_button_press)
+  $('#modifier').hover(() => {
+    if (Vars.points >= Vars.modifier.cost) {
+      $(this).css('cursor', 'pointer')
+    } else {
+      $(this).css('cursor', 'default')
+    }
+  })
+  $('#autoclick').hover(() => {
+    if (Vars.points >= Vars.modifier.cost) {
+      $(this).css('cursor', 'pointer')
+    } else {
+      $(this).css('cursor', 'default')
+    }
+  })
 })
 
 var Vars = { }
 
+function updateButtonCost () {
+  if (Vars.points >= Vars.modifier.cost) {
+    $('#modifier').css('background-color', 'rgb(200,127,127)')
+  } else {
+    $('#modifier').css('background-color', 'gray')
+  }
+
+  if (Vars.points >= Vars.autoclick.cost) {
+    $('#autoclick').css('background-color', 'rgb(200,127,127)')
+  } else {
+    $('#autoclick').css('background-color', 'gray')
+  }
+}
+
+function updateDisplayVariables () {
+  $('#points_numeric').text(Vars.points)
+  $('#modifiers_cost').text(Vars.modifier.cost)
+  $('#current_modifiers').text(Vars.modifier.current)
+  $('#count_of_autoclickers').text(Vars.autoclick.agents.length)
+  $('#autoclick_cost').text(((Vars.autoclick.cost.toFixed(3) * 1000)/1000).toString())
+}
+
 function game_init () {
-  Vars.modifier = { current: 1, cost: 100, increment: 0.1 }
-  Vars.autoclick = { agents: [], cost: 1 }
+  Vars.modifier = { current: 1, cost: 100, increment: 0.5 }
+  Vars.autoclick = { agents: [], cost: 100 }
   Vars.points = 0
   Vars.per_click = 1
+  updateDisplayVariables()
 }
 
 function increaseCost (cost) {
+  return Math.floor(Math.pow((0.018 * cost + 2), 3) + 100)
+}
 
+function increaseAutoclickCost () {
+  Vars.autoclick.cost += (Vars.modifier.cost / 100)
+  updateButtonCost()
+  updateDisplayVariables()
 }
 
 const generic_button_press = function () {
-  switch(this.id){
-    case "modifier":
+  switch (this.id) {
+    case 'modifier':
       let cost = Vars.modifier.cost
-      if (Vars.points > cost) {
+      if (Vars.points >= cost) {
+        console.log('modifier clicked')
         Vars.points -= cost
         Vars.modifier.current += Vars.modifier.increment
         Vars.modifier.cost = increaseCost(cost)
+        updateButtonCost()
+        updateDisplayVariables()
       }
       break
-    case "autoclick":
+    case 'autoclick':
+      console.log('autoclick clicked')
+      if (Vars.points >= Vars.autoclick.cost) {
+        let agent = new AutoclickAgent()
+        agent.act()
+        Vars.autoclick.agents.push(agent)
+        Vars.points -= Vars.autoclick.cost
+        updateButtonCost()
+        updateDisplayVariables()
+      }
       break
-    case "per_click":
+    case 'per_click':
+      console.log('per_click clicked')
+      Vars.points += Vars.modifier.current * Vars.per_click * 100
+      updateButtonCost()
+      updateDisplayVariables()
   }
 }
