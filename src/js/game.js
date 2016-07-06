2e1f404090346874189bad4a94500fb31053f34e
robbiemu <roberto.tomas.cuentas@gmail.com>
2016-07-05 20:32:51 -0400

    daimen's game logic beginning changes

--- src/js/game.js
+++ src/js/game.js
@@ -8,16 +8,25 @@ $().ready(() => {
 var Vars = { }
 
 function game_init () {
-  Vars.modifier = { current: 1, cost: 1, increment: 0.1 }
+  Vars.modifier = { current: 1, cost: 100, increment: 0.1 }
   Vars.autoclick = { agents: [], cost: 1 }
   Vars.points = 0
   Vars.per_click = 1
 }
 
+function increaseCost (cost) {
+
+}
+
 const generic_button_press = function () {
   switch(this.id){
     case "modifier":
-
+      let cost = Vars.modifier.cost
+      if (Vars.points > cost) {
+        Vars.points -= cost
+        Vars.modifier.current += Vars.modifier.increment
+        Vars.modifier.cost = increaseCost(cost)
+      }
       break
     case "autoclick":
       break
